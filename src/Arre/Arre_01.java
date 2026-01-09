33f5fd143d8173baccc642b2f8e1e511f20e575f
vishal maddeshiya <vishalkumar78044@gmail.com>
2026-01-09 05:51:59 +0530

    first commit

--- src/Arre/Arre_01.java
+++ src/Arre/Arre_01.java
@@ -3,12 +3,14 @@ package Arre;
 public class Arre_01 {
     public static void main(String[] args) {
         // Arr is mutabile
+        System.out.println("vishal kumar");
         int[] x = {6,19,9,5,7,67};
         //indexing
         System.out.println(x[0]);
         // updating elements
         System.out.println(x[3]);
         x[3] = 89;
+
         System.out.println(x[3]);
     }
 }
